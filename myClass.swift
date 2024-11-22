c9c7e835f1e631a5ba47fe482ace81f95d84f641
Becerril <cfeapps@gmail.com>
2024-11-22 09:46:02 +0100

    refactor code

--- myClass.swift
+++ myClass.swift
@@ -2,8 +2,6 @@ class MyClass {
 let myProperty = "" 
 let otherProperty: Int = 5
 let myProperty2 = "Hola"
-<<<<<<< HEAD
-let user = ""
-=======
->>>>>>> ef2b7fb... resolve conflict
+let isUserLogin = true
+let user = "User"
 }
